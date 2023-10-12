6f4025ab837cdc720614f1ef83dadefe1ea73c6b
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-10-12 16:40:06 +0530

    model folder added

--- API/index.js
+++ API/index.js
@@ -1,7 +1,18 @@
 const express=require('express')
+const { default: mongoose } = require('mongoose')
 const app=express()
 require('dotenv').config()
 
+
+const connect=async()=>{
+try {
+    await mongoose.connect(process.env.URL)
+    console.log("Connected to DB");
+} catch (error) {
+    console.log(error);
+}
+}
 app.listen(3000,()=>{
+    connect()
     console.log("Server is listening!!!");
 })
